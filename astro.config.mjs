94dd84308dd96eb3d4d32693242c780c84362437
Caleb Fringer <cfringer98@gmail.com>
2026-06-28 19:28:17 -0700

    Add Solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://caleb-fringer.github.io",
-    server: {
-        port: 3000,
-    },
+  site: "https://caleb-fringer.github.io",
+
+  server: {
+      port: 3000,
+  },
+
+  integrations: [solidJs()],
 });
